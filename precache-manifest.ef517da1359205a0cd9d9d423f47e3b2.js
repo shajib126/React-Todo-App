16501e8fec4eb4dffde1d07d57563d0e7a4873a1
shajib126 <shajib126@gmail.comm>
2020-06-28 22:19:50 +0600

    Updates

---
+++ precache-manifest.ef517da1359205a0cd9d9d423f47e3b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6742e159afd2548feb726310d2a8b5f",
+    "url": "/React-Todo-App/index.html"
+  },
+  {
+    "revision": "e4fb8bda1b3e2b1c80e6",
+    "url": "/React-Todo-App/static/css/main.2909daf2.chunk.css"
+  },
+  {
+    "revision": "fcd8ac49c09c83bdee95",
+    "url": "/React-Todo-App/static/js/2.f0e02ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-App/static/js/2.f0e02ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4fb8bda1b3e2b1c80e6",
+    "url": "/React-Todo-App/static/js/main.5d11eae2.chunk.js"
+  },
+  {
+    "revision": "75530a069d22f8b0de78",
+    "url": "/React-Todo-App/static/js/runtime-main.bd280751.js"
+  }
+]);
